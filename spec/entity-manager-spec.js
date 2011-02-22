d3004e315088d724ab2d518034a0b734a84f4e99
Hanno Braun <hb@hannobraun.com>
2011-02-22 16:42:24 +0100

    Refactoring: Rewrote EntityManager to improve its API.

--- spec/entity-manager-spec.js
+++ spec/entity-manager-spec.js
@@ -1,88 +1,91 @@
 
 describe( "EntityManager", function() {
 
-	var componentX1 = {
-		x: "x1"
-	};
-	var componentX2 = {
-		x: "x2"
-	};
-	var componentY1 = {
-		y: "y1"
-	};
-	var componentY2 = {
-		y: "y2"
-	};
-
 	var entityManager = null;
+	var exampleSystem = null;
+	
+	// components
+	var a1 = "a1";
+	var a2 = "a2";
+	var b1 = "b1";
+	var b2 = "b2";
+	var c1 = "c1";
+	var c2 = "c2";
+	var d1 = "d1";
+	var d2 = "d2";
 	
 	beforeEach( function() {
 		entityManager = new EntityManager();
 		
-		entityManager.defineEntity( "entity1", {
-			componentX: componentX1,
-			componentY: componentY1
+		entityManager.createEntity( {
+			a: a1,
+			b: b1
 		} );
-		entityManager.defineEntity( "entity2", {
-			componentX: componentX2,
-			componentY: componentY2
+		entityManager.createEntity( {
+			a: a2,
+			b: b2
 		} );
-	} );
-
-	it( "should return all components of a specific type.", function () {
-		var result = entityManager.componentsByType( [ "componentX" ] );
-		
-		expect( result.entities ).toEqual( [ "entity1", "entity2" ] );
-		expect( result.components[ "componentX" ] ).toEqual( [ componentX1, componentX2 ] );
-	} );
-	
-	it( "should return only those entities that actually have the specified component.", function() {
-		entityManager.defineEntity( "entity3", {
-			component2: {
-				y: "y3"
-			}
+		entityManager.createEntity( {
+			c: c1,
+			d: d1
+		} );
+		entityManager.createEntity( {
+			c: c2,
+			d: d2,
 		} );
 		
-		var result = entityManager.componentsByType( [ "componentX" ] );
-		
-		expect( result.entities ).toEqual( [ "entity1", "entity2" ] );
-		expect( result.components[ "componentX" ] ).toEqual( [ componentX1, componentX2 ] );
+		exampleSystem = {
+			processComponents: function() {},
+			updateComponents: function() {}
+		};
 	} );
 	
-	it( "should allow for adding a component to an entity.", function() {
-		var newComponent = {
-			z: "z1"
-		}
-		entityManager.addComponentToEntity( "componentZ", newComponent, "entity1" );
-		
-		var result = entityManager.componentsByType( [ "componentZ" ] );
-		
-		expect( result.entities ).toEqual( [ "entity1" ] );
-		expect( result.components[ "componentZ" ] ).toEqual( [ newComponent ] );
-	} );
-	
-	it( "should only return those entities that have all of the specified components.", function() {
-		entityManager.defineEntity( "entity3", {
-			componentX: {
-				x: "x3"
-			}
+	describe( "processComponents", function() {
+		beforeEach( function() {
+			spyOn( exampleSystem, "processComponents" );
 		} );
 		
-		var result = entityManager.componentsByType( [ "componentX", "componentY" ] );
+		it( "should pass requested components to a system.", function() {
+			entityManager.processComponents( "a", "b" ).using( exampleSystem );
 		
-		expect( result.entities ).toEqual( [ "entity1", "entity2" ] );
-		expect( result.components[ "componentX" ] ).toEqual( [ componentX1, componentX2 ] );
-		expect( result.components[ "componentY" ] ).toEqual( [ componentY1, componentY2 ] );
-	} );
-	
-	it( "should find a component, event though its value is 0.", function() {
-		entityManager.defineEntity( "entity3", {
-			numberComponent: 0
+			expect( exampleSystem.processComponents ).toHaveBeenCalledWith( [ a1, a2 ], [ b1, b2 ] );
 		} );
 		
-		var result = entityManager.componentsByType( [ "numberComponent"  ] );
+		it( "should pass additional parameters to a system.", function() {
+			var p1 = "p1";
+			var p2 = "p2";
+			
+			entityManager.processComponents( "a", "b" )
+					.withParameters( p1, p2 )
+					.using( exampleSystem );
 		
-		expect( result.entities ).toEqual( [ "entity3" ] );
-		expect( result.components[ "numberComponent" ] ).toEqual( [ 0 ] );
+			expect( exampleSystem.processComponents ).toHaveBeenCalledWith( p1, p2, [ a1, a2 ], [ b1, b2 ] );
+		} );
+	
+		it( "should pass several sets of components to a system.", function() {
+			entityManager.processComponents( "a", "b" )
+					.and( "c", "d" )
+					.using( exampleSystem );
+		
+			expect( exampleSystem.processComponents )
+					.toHaveBeenCalledWith( [ a1, a2 ], [ b1, b2 ], [ c1, c2 ], [ d1, d2 ] );
+		} );
+	} );
+	
+	describe( "updateComponents", function() {
+		it( "should replace the entity's components with the updated ones returned by the system.", function() {
+			// updated components
+			var a1New = "a1+";
+			var a2New = "a2+";
+			var b1New = "b1+";
+			var b2New = "b2+";
+			var updatedComponents = [ [ a1New, a2New ], [ b1New, b2New ] ];
+			
+			spyOn( exampleSystem, "updateComponents" ).andReturn( updatedComponents );
+			
+			entityManager.updateComponents( "a", "b" ).using( exampleSystem );
+			
+			expect( entityManager.entities.components( [ "a", "b" ] ) ).toEqual( updatedComponents );
+		} );
 	} );
 } );
--- src/entity-manager.js
+++ src/entity-manager.js
@@ -1,37 +1,62 @@
 
 function EntityManager() {
-	this.entities = []
+	this.entities = new Entities();
 }
 
-EntityManager.prototype.defineEntity = function( entityName, entity ) {
-	entity.id = entityName;
+EntityManager.prototype.createEntity = function( entity ) {
+	this.entities.add( entity );
+}
+
+EntityManager.prototype.processComponents = function() {
+	var components = this.entities.components( arguments );
+	return new ComponentProcessor( this.entities, components );
+}
+
+EntityManager.prototype.updateComponents = function() {
+	var components = this.entities.components( arguments );
+	var entitiesToUpdate = this.entities.entitiesWith( arguments );
+	var componentsToUpdate = arguments;
+	return new ComponentProcessor( this.entities, components, entitiesToUpdate, componentsToUpdate );
+}
+
+
+
+function Entities() {
+	this.nextId = 0;
+	this.entities = [];
+}
+
+Entities.prototype.add = function( entity ) {
+	entity.id = this.nextId++;
 	this.entities.push( entity );
 }
 
-EntityManager.prototype.addComponentToEntity = function( componentName, component, entityId ) {
+Entities.prototype.update = function( entityId, componentType, component ) {
 	var entity = this.entities.filter( function( entity ) {
 		return entity.id == entityId;
 	} )[ 0 ];
-	
-	entity[ componentName ] = component;
+	entity[ componentType ] = component;
 }
 
-EntityManager.prototype.componentsByType = function( componentTypes ) {
-	var entitiesWithTheseComponentTypes = this._findEntitiesWithTheseComponents( componentTypes );
-	var entityIds = this._extractIdsFromEntities( entitiesWithTheseComponentTypes );
-	var components = this._extractComponentsFromEntities( componentTypes, entitiesWithTheseComponentTypes );
-	
-	return {
-		entities: entityIds,
-		components: components
-	};
+Entities.prototype.entitiesWith = function( componentTypes ) {
+	var entitiesWithTheseComponents = this._entitiesWithTheseComponents( componentTypes );
+	return entitiesWithTheseComponents.map( function( entity ) {
+		return entity.id;
+	} );
 }
 
-EntityManager.prototype._findEntitiesWithTheseComponents = function( componentTypes ) {
+Entities.prototype.components = function( componentTypes ) {
+	var entitiesWithTheseComponents = this._entitiesWithTheseComponents( componentTypes );
+	var components = this._extractComponentsFromEntities( componentTypes, entitiesWithTheseComponents );
+	return components;
+}
+
+Entities.prototype._entitiesWithTheseComponents = function( componentTypes ) {
 	return this.entities.filter( function( entity ) {
 		var hasAllComponents = true;
 		for ( var i = 0; i < componentTypes.length; i++ ) {
-			if ( entity[ componentTypes[ i ] ] == undefined ) {
+			var componentType = componentTypes[ i ];
+			if ( entity[ componentType ] == undefined ) {
 				hasAllComponents = false;
 			}
 		}
@@ -39,18 +64,88 @@ EntityManager.prototype._findEntitiesWithTheseComponents = function( componentTy
 	} );
 }
 
-EntityManager.prototype._extractIdsFromEntities = function( entities ) {
-	return entities.map( function( entity ) {
-		return entity.id;
-	} );
+Entities.prototype._extractComponentsFromEntities = function( componentTypes, entities ) {
+	var allComponents = [];
+	
+	for ( var i = 0; i < componentTypes.length; i++ ) {
+		var componentType = componentTypes[ i ];
+		var components = entities.map( function( entity ) {
+			return entity[ componentType ];
+		} );
+		allComponents.push( components );
+	}
+	
+	return allComponents;
 }
 
-EntityManager.prototype._extractComponentsFromEntities = function( componentTypes, entities ) {
-	var components = {};
-	for ( var i = 0; i < componentTypes.length; i++ ) {
-		components[ componentTypes[ i ] ] = entities.map( function( entity ) {
-			return entity[ componentTypes[ i ] ];	
-		} );
+
+
+function ComponentProcessor( entities, components, entitiesToUpdate, componentsToUpdate ) {
+	this.entities = entities;
+	this.components = components;
+	this.entitiesToUpdate = entitiesToUpdate;
+	this.componentsToUpdate = componentsToUpdate;
+	
+	this.additionalParameters = [];
+}
+
+ComponentProcessor.prototype.using = function( system ) {
+	var parameters = this.additionalParameters.concat( this.components );
+	
+	if ( this.entitiesToUpdate && this.componentsToUpdate ) {
+		var result = system.updateComponents.apply( system, parameters );
+	
+		for ( var entityIndex = 0; entityIndex < this.entitiesToUpdate.length; entityIndex++ ) {
+			for ( var componentIndex = 0; componentIndex < this.componentsToUpdate.length; componentIndex++ ) {
+				var entityId = this.entitiesToUpdate[ entityIndex ];
+				var componentType = this.componentsToUpdate[ componentIndex ];
+				var component = result[ componentIndex ][ entityIndex ];
+				this.entities.update( entityId, componentType, component );
+			}
+		}
 	}
-	return components;
+	else {
+		system.processComponents.apply( system, parameters );
+	}
+}
+
+ComponentProcessor.prototype.withParameters = function() {
+	// Since arguments is just array-like, not a real Array, we convert it here. Otherwise it's just a hassle later on. 
+	for ( var i = 0; i < arguments.length; i++ ) {
+		this.additionalParameters.push( arguments[ i ] );
+	}
+	
+	return this;
+}
+
+ComponentProcessor.prototype.and = function() {
+	var additionalComponents = this.entities.components( arguments );
+	this.components = this.components.concat( additionalComponents );
+	return this;
+}
+
+
+
+// Some scaffolding to keep compatibility with the previous API.
+EntityManager.prototype.defineEntity = function( id, entity ) {
+	this.createEntity( entity );
+}
+
+EntityManager.prototype.componentsByType = function( componentTypes ) {
+	var entityIds = this.entities.entitiesWith( componentTypes );
+	var components = this.entities.components( componentTypes );
+	
+	var result = {
+		entities: entityIds,
+		components: {}
+	}
+	for ( var i = 0; i < componentTypes.length; i++ ) {
+		result.components[ componentTypes[ i ] ] = components[ i ];
+	}
+	
+	return result;
+}
+
+EntityManager.prototype.addComponentToEntity = function( componentType, component, entityId ) {
+	this.entities.update( entityId, componentType, component );
 }
